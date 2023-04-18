1ad6c1f7e0b75a1ee1d03d55e4d68b8392039475
douwe <douwe0303@gmail.com>
2023-04-18 16:28:48 +0200

    Removed unnecessary fields

--- src/app/transformer/order-transformer/order-transformer.spec.ts
+++ src/app/transformer/order-transformer/order-transformer.spec.ts
@@ -1,5 +1,4 @@
 import { OrderTransformer } from './order-transformer';
-import {LeasecarTransformer} from "../leasecar-transformer/leasecar-transformer";
 
 describe('OrderTransformer', () => {
   it('should create an instance', () => {
